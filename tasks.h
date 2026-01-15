b55d3dba04b2afe7e66058e678e3893112868c1d
Denis1818 <permpdv@gmail.com>
2026-01-15 11:41:53 +0500

    Update and rename Лаб2.cpp to tasks.h

---
+++ tasks.h
@@ -0,0 +1,25 @@
+#ifndef TASKS_H
+#define TASKS_H
+
+#include <algorithm>
+#include <deque>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <locale>
+
+void Task1();
+void Task2();
+void Task3();
+void Task4();
+void Task5();
+void Task6();
+void Task7();
+void Task8();
+void ShowMenu();
+
+#endif // TASKS_H
